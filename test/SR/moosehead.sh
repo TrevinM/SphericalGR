eda38836eab70a1b1109b07ed7c32d4b4092c90c
Trevin <trevin.macomber@gmail.com>
2026-07-15 16:07:10 -0400

    Move test files into seperate repo

--- test/SR/moosehead.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --mail-type=BEGIN,END,FAIL
-
-export OMP_NUM_THREADS=16
-
-./SphericalGR Input
--- test/example/.example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --mail-type=BEGIN,END,FAIL
-
-export OMP_NUM_THREADS=16
-if [ ! -d output ]; then mkdir output; fi
-rm SphericalGR
-cp ../../build/SphericalGR .
-
-./SphericalGR Input
